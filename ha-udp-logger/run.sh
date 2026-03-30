869c1ef5306e60270cf50820ed1ace35835d0b32
sclsat <seclsat@gmail.com>
2026-03-30 11:45:57 +0300

    Update run.sh

--- ha-udp-logger/run.sh
+++ ha-udp-logger/run.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 
-set -e
-
+# Читаем конфигурацию
 CONFIG_PATH=/data/options.json
 LOG_DIR=$(bashio::config 'log_dir')
 MAX_SIZE_MB=$(bashio::config 'max_size_mb')
 ROTATE_COUNT=$(bashio::config 'rotate_count')
 LOG_LEVEL=$(bashio::config 'log_level')
 
+# Логируем
 bashio::log.info "Запуск UDP Log Collector"
 bashio::log.info "Директория логов: ${LOG_DIR}"
 bashio::log.info "Максимальный размер: ${MAX_SIZE_MB}MB"
 bashio::log.info "Количество ротаций: ${ROTATE_COUNT}"
 bashio::log.info "Уровень логирования: ${LOG_LEVEL}"
 
-# Запускаем Python скрипт
+# Запускаем скрипт
 exec python3 /udp_logger.py \
     --log-dir "${LOG_DIR}" \
     --max-size-mb "${MAX_SIZE_MB}" \
